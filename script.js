0e6ceabf1d7cc3f28ced178949148e9321a401c0
Negash <negash@cca.edu>
2024-02-09 23:57:11 -0800

    PWA BBY

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+let bus19Data = [];
+let bus48Data = [];
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('bus19').addEventListener('click', () => {
-        fetchBusTimes('14128');
+        displayBusTimes(bus19Data, 'bus19');
         toggleButtonState('bus19');
     });
     document.getElementById('bus48').addEventListener('click', () => {
-        fetchBusTimes('13507');
+        displayBusTimes(bus48Data, 'bus48');
         toggleButtonState('bus48');
     });
+
+    // Fetch data for both buses on initial load
+    fetchBusTimes('14128', 'bus19');
+    fetchBusTimes('13507', 'bus48');
 });
 
-function fetchBusTimes(stopId) {
+function fetchBusTimes(stopId, busId) {
     const API_ENDPOINT = `https://api.511.org/transit/StopMonitoring?api_key=8ed0464e-6f76-440e-8038-6f300c697f7c&agency=SF&stopCode=${stopId}`;
     fetch(API_ENDPOINT)
         .then(response => {
@@ -20,7 +27,12 @@ function fetchBusTimes(stopId) {
         })
         .then(data => {
             const monitoredStopVisits = data.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit;
-            displayBusTimes(monitoredStopVisits);
+            if (busId === 'bus19') {
+                bus19Data = monitoredStopVisits;
+            } else {
+                bus48Data = monitoredStopVisits;
+            }
+            displayBusTimes([...bus19Data, ...bus48Data]);
         })
         .catch(error => {
             console.error('Error fetching bus times:', error);
@@ -35,7 +47,7 @@ function toggleButtonState(selectedButtonId) {
     document.getElementById(selectedButtonId).classList.add('selected');
 }
 
-function displayBusTimes(monitoredStopVisits) {
+function displayBusTimes(monitoredStopVisits, selectedBusId) {
     const busTimesElement = document.getElementById('busTimes');
     busTimesElement.innerHTML = '';
 
@@ -50,32 +62,30 @@ function displayBusTimes(monitoredStopVisits) {
         const destination = journey.DestinationName;
         const expectedArrival = journey.MonitoredCall.ExpectedArrivalTime;
 
-        
-
         const arrivalTimeDate = new Date(expectedArrival);
         const timeFromNow = Math.round((arrivalTimeDate - new Date()) / 60000);
         const displayTimeFromNow = timeFromNow < 0 ? "Arrived" : `${timeFromNow} min`;
 
         const timeElement = document.createElement('div');
-        timeElement.classList.add('bus-time-entry', 'flex', 'flex-col', 'sm:flex-row', 'items-center', 'justify-center', 'sm:justify-between', 'bg-white', 'shadow', 'rounded', 'mb-6', 'p-1', 'w-full');
+        timeElement.classList.add('bus-time-entry', 'flex', 'flex-col', 'sm:flex-row', 'items-center', 'justify-center', 'sm:justify-between', 'bg-white', 'shadow', 'rounded', 'mb-6', 'p-', 'w-full');
         timeElement.style.padding = '1.5rem'; // This applies padding to all sides
 
         timeElement.innerHTML = `
-        <div class="line text-7xl mb-2 sm:mb-0" style="min-width: 80px;">
-            <span class="font-bold">${lineRef}</span>
-        </div>
-        <div class="details flex-grow flex flex-col sm:flex-row items-center justify-center sm:justify-start text-center">
-            <div class="destination text-5xl sm:text-5xl mb-2 sm:mb-0 sm:mr-2" style="min-width: 150px;"> <!-- Updated text size -->
-                <span>${destination}</span>
+            <div class="line text-7xl mb-2 sm:mb-0" style="min-width: 80px;">
+                <span class="font-bold">${lineRef}</span>
             </div>
-            <div class="arrival-time text-2xl mb-2 sm:mb-0 sm:mr-2" style="min-width: 120px;">
-                <span>${arrivalTimeDate.toLocaleTimeString()}</span>
+            <div class="details flex-grow flex flex-col sm:flex-row items-center justify-center sm:justify-start text-center">
+                <div class="destination text-5xl sm:text-5xl mb-2 sm:mb-0 sm:mr-2" style="min-width: 150px;">
+                    <span>${destination}</span>
+                </div>
+                <div class="arrival-time text-2xl mb-2 sm:mb-0 sm:mr-2" style="min-width: 120px;">
+                    <span>${arrivalTimeDate.toLocaleTimeString()}</span>
+                </div>
             </div>
-        </div>
-        <div class="time-from-now text-3xl md:text-2xl mb-2 sm:mb-0" style="min-width: 80px;">
-            <span class="font-bold">${displayTimeFromNow}</span>
-        </div>
-    `;
+            <div class="time-from-now text-6xl md:text-6xl mb-2 sm:mb-0" style="min-width: 80px;">
+                <span class="font-bold">${displayTimeFromNow}</span>
+            </div>
+        `;
         busTimesElement.appendChild(timeElement);
     });
 }
